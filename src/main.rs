fb15863fe836fe02c10d9b27e7cd56306e2984c3
yobibyte <vitaliykurin@gmail.com>
2024-01-30 22:30:02 +0000

    Read config.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::fs;
+use homedir::get_my_home;
+
 fn main() {
-    println!("Hello, world!");
+    // I have no idea what's going on and why we need to unwrap twice.
+    // I am also surprised that to get your home directory, you need a crate.
+    let config_path = get_my_home().unwrap().unwrap().join(".rtd");
+    // TODO: create a file if it does not exist.
+    let contents = fs::read_to_string(config_path.as_path()).expect("");
+    // TODO: add index.md if it does not exist.
+    println!("{contents}")
 }
