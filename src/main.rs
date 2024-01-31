a3e6f66e1b45009b232509d901dc3bb34235479e
yobibyte <vitaliykurin@gmail.com>
2024-01-31 01:02:09 +0000

    Create index.md if it does not exist.

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,38 @@
 use std::fs;
+use std::io::Write;
 use homedir::get_my_home;
+use std::path::Path;
 
 const CONFIG_FNAME: &str = ".rtd";
+const INBOX_FNAME: &str = "inbox.md";
 const RTD_ROOT_VAR_NAME: &str = "RTD_ROOT";
 
-fn main() {
+// fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     // I have no idea what's going on and why we need to unwrap twice.
     // I am also surprised that to get your home directory, you need a crate.
-    let config_path = get_my_home().unwrap().unwrap().join(CONFIG_FNAME);
+    let config_path = get_my_home().unwrap().unwrap();
     if config_path.exists() {
         // When config will grow, we'll need to read file line by line.
-        let contents = fs::read_to_string(config_path.as_path()).expect("");
-        // TODO: add index.md if it does not exist.
+        let contents = fs::read_to_string(config_path.join(CONFIG_FNAME)).expect("");
+
         println!("{contents}");
         let line: Vec<_> = contents.split("=").collect();
         if line[0] != RTD_ROOT_VAR_NAME {
             println!("You need to have {RTD_ROOT_VAR_NAME}=<absolute_path> in the config.");
         }
-        let rtd_root = line[1];
+        let rtd_root = line[1].strip_suffix('\n').expect("");
         println!("Using rtd root: {rtd_root}");
+
+        let root_path =  Path::new(rtd_root);
+        let inbox_path =  root_path.join(INBOX_FNAME);
+        if !root_path.join(INBOX_FNAME).exists() {
+            println!("There is no {INBOX_FNAME} file in the root. Creating...");
+            let mut f = fs::File::create(root_path.join(INBOX_FNAME))?;
+            f.write_all("".as_bytes())?;
+        }
     } else {
         println!("You need to create a config at ~/{CONFIG_FNAME} and add GTD_DIR=<rtd_root_dir_absolute_path> there.");
     }
+    Ok(())
 }
